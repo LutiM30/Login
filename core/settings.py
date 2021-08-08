83c0ebcf567d0956126b766addbc3adcdb8e616d
Mitul Mistry <62054801+LutiM30@users.noreply.github.com>
2021-08-09 00:20:20 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -150,6 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mitmistry311@gmail.com'
-EMAIL_HOST_PASSWORD = '30OkvwN11NwvkO30'
+EMAIL_HOST_PASSWORD = '****'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
